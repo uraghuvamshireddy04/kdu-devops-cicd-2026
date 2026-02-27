4741f77a30f77090ae096de5c0af6fb5d8935972
U Raghu Vamshi Reddy <raghu.reddy@kickdrum.com>
2026-02-27 17:14:41 +0530

    first commit

---
+++ src/main/java/com/example/cicd/controller/VersionController.java
@@ -0,0 +1,24 @@
+package com.example.cicd.controller;
+
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VersionController {
+  private final BuildProperties buildProperties;
+
+    @GetMapping("/")
+    public String hello(){
+        return "Hello";
+    }
+    
+  public VersionController(BuildProperties buildProperties) {
+    this.buildProperties = buildProperties;
+  }
+
+  @GetMapping("/version")
+  public String version() {
+    return buildProperties.getVersion();
+  }
+}
